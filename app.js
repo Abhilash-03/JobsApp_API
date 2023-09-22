6b77d4e265c052eda967a9d3436be4589002e80c
Abhilash <abhilashjha335@gmail.com>
2023-09-22 16:09:49 +0530

    add security packages

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 require('dotenv').config();
 require('express-async-errors');
 
+// extra security packages
+const helmet = require('helmet');
+const cors = require('cors');
+const xss = require('xss-clean');
+const rateLimiter = require('express-rate-limit');
+
 const express = require('express');
 const notFound = require('./middleware/notFound');
 const errorHandler = require('./middleware/errorHandler');
@@ -13,23 +19,32 @@ const authenticated = require('./middleware/authentication');
 
 const PORT = process.env.PORT || 3000;  
 
+app.set('trust proxy', 1);
+app.use(rateLimiter({
+    windowMs : 15 * 60 * 1000,
+    max : 100 // limit each IP to 100 requests per windowMs
+}))
 app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 
+app.get('/', (req, res)=>{
+    res.send('<h1>Jobs API</h1>');
+})
+
+// Routes
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/jobs', authenticated, jobRouter);
 
-app.use(notFound);
+// Middleware
 app.use(errorHandler);   
-
-
-app.get('/', (req, res) => {
-    res.send("Jobs app API");
-})
+app.use(notFound);
 
 const start = async() => {
     try{
         await connectDB(process.env.MONGO_URI);
-        app.listen(PORT, console.log(`Server listening on port ${PORT}...`));
+        app.listen(PORT, ()=> console.log(`Server listening on port ${PORT}...`));
     }
     catch(error){
         console.log(error);
